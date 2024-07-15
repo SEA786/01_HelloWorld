cdd4a90cf1f9231ad7aa2aba41f17985ff26e2ca
SEA786 <shams.eltaf786@gmail.com>
2024-07-15 10:49:39 +0200

    App + Cat

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,12 +1,25 @@
 package com.cc.java;
 
-public class App 
-{
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+public class App {
     
+    public static void main(String[] args) {
+
+        // Instanziierung  --> Erzeugen eines Objektes
+        // System.out.println(new Cat());
+
+        // cat --> Referenzvariable
+        // Cat --> Referenztyp(Klasse)
+        Cat cat = new Cat();
+        System.out.println("Blick von aussen: " + cat);
+        cat.tellYourAddress();
+
+        System.out.println("----------------------------");
+
+        Cat cat2 = new Cat();
+        System.out.println("Blick von aussen: " + cat2);
+        cat2.tellYourAddress();
+
+    }
+
+   
 }
-
-
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,12 @@
+package com.cc.java;
+
+public class Cat 
+{
+
+    // this -- Instanzvariable
+    void tellYourAddress(){
+        System.out.println("Blick von innen: " + this);
+    }
+
+
+}
